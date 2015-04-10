4a790b71b2c56e64c6ceb764768358b5d7690a8d
Jared <jguardin@calpoly.edu>
2015-04-10 15:31:54 -0700

    Fixed

---
+++ worldmodel.py
@@ -0,0 +1,124 @@
+import entities
+import pygame
+import ordered_list
+import actions
+import occ_grid
+import point
+
+class WorldModel:
+    def __init__(self, num_rows, num_cols, background):
+      self.background = occ_grid.Grid(num_cols, num_rows, background)
+      self.num_rows = num_rows
+      self.num_cols = num_cols
+      self.occupancy = occ_grid.Grid(num_cols, num_rows, None)
+      self.entities = []
+      self.action_queue = ordered_list.OrderedList()
+      
+    def add_entity(self, entity):
+       pt = entity.get_position()
+       if self.within_bounds(pt):
+          old_entity = occ_grid.get_cell(self.occupancy, pt)
+          if old_entity != None:
+             old_entity.clear_pending_actions()
+          occ_grid.set_cell(self.occupancy, pt, entity)
+          self.entities.append(entity)
+
+
+    def within_bounds(self, pt):
+       return (pt.x >= 0 and pt.x < self.num_cols and
+          pt.y >= 0 and pt.y < self.num_rows)
+
+
+    def is_occupied(self, pt): # isinstance
+       return (self.within_bounds(pt) and
+          occ_grid.get_cell(self.occupancy, pt) != None)
+          
+    def find_nearest(self, pt, type):
+       oftype = [(e, pt.distance_sq(e.get_position()))
+          for e in self.entities if isinstance(e, type)]
+
+       return nearest_entity(oftype)
+       
+    def move_entity(self, entity, pt):
+        tiles = []
+        if self.within_bounds(pt):
+            old_pt = entity.get_position()
+            occ_grid.set_cell(self.occupancy, old_pt, None)
+            tiles.append(old_pt)
+            occ_grid.set_cell(self.occupancy, pt, entity)
+            tiles.append(pt)
+            entity.set_position(pt)
+
+        return tiles
+        
+    def remove_entity(self, entity):
+        self.remove_entity_at(entity.get_position())
+        
+    def remove_entity_at(self, pt):
+        if (self.within_bounds(pt) and
+            occ_grid.get_cell(self.occupancy, pt) != None):
+            entity = occ_grid.get_cell(self.occupancy, pt)
+            entity.set_position(point.Point(-1, -1))
+            self.entities.remove(entity)
+            occ_grid.set_cell(self.occupancy, pt, None)
+        
+    def schedule_action(self, action, time):
+        self.action_queue.insert(action, time)
+
+
+    def unschedule_action(self, action):
+        self.action_queue.remove(action)
+        
+        
+        
+    def update_on_time(self, ticks):
+        tiles = []
+
+        next = self.action_queue.head()
+        while next and next.ord < ticks:
+            self.action_queue.pop()
+            tiles.extend(next.item(ticks))  # invoke action function
+            next = self.action_queue.head()
+
+        return tiles
+        
+        
+    def get_background_image(self, pt): #worldview
+        if self.within_bounds(pt):
+            return occ_grid.get_cell(self.background, pt).get_image()
+
+
+    def get_background(self, pt):
+        if self.within_bounds(self, pt):
+            return occ_grid.get_cell(self.background, pt)
+
+
+    def set_background(self, pt, bgnd):
+        if self.within_bounds(pt):
+            occ_grid.set_cell(self.background, pt, bgnd)
+                
+    def get_tile_occupant(self, pt): #worldview
+        if self.within_bounds(pt):
+            return occ_grid.get_cell(self.occupancy, pt)
+
+    def get_entities(self):
+        return self.entities
+        
+   
+   
+def nearest_entity(entity_dists):
+   if len(entity_dists) > 0:
+      pair = entity_dists[0]
+      for other in entity_dists:
+         if other[1] < pair[1]:
+            pair = other
+      nearest = pair[0]
+   else:
+      nearest = None
+
+   return nearest
+
+
+
+
+
