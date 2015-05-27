ef649129a7a98f07e4865e6694086949499c732b
willle3456@gmail.com <wdle@csclnx03.ad.calpoly.edu>
2015-05-27 13:16:28 -0700

    A* is working, but nothing moves after it runs

--- 102ProjectJava/MobileAnimatedActor.java
+++ 102ProjectJava/MobileAnimatedActor.java
@@ -1,32 +1,170 @@
-import processing.core.PImage;
+import processing.core.*;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+
 import static java.lang.Math.abs;
 
 public abstract class MobileAnimatedActor
    extends AnimatedActor
 {
+	private PApplet p = new PApplet(); 
    public MobileAnimatedActor(String name, Point position, int rate,
       int animation_rate, List<PImage> imgs)
    {
       super(name, position, rate, animation_rate, imgs);
    }
+   
+   public int distance(Point currPt, Point goalPt)
+   {
+	   return Math.abs((currPt.x - goalPt.x) + (currPt.y - goalPt.y));
+   }
+   
+   public List<Point> getAdjacentPts(WorldModel world, Point pt, Point goalPt)
+   {
+	   List<Point> temp = new LinkedList<Point>();
+	   List<Point> adj = new LinkedList<Point>();
+	   temp.add(new Point(pt.x - 1, pt.y));
+	   temp.add(new Point(pt.x + 1, pt.y));
+	   temp.add(new Point(pt.x, pt.y + 1));
+	   temp.add(new Point(pt.x, pt.y - 1));
+	   
+	   for(Point pt1 : temp)
+	   {
+		   if((world.withinBounds(pt1) && canPassThrough(world, pt1)) || (goalPt.x == pt1.x && goalPt.y == pt1.y))
+		   {
+			   adj.add(pt1); 
+		   }
+	   }
+	   
+	   return adj; 
+   }
+   
+   public int getNodeValue(Point startPt, Point goalPt, Point currPt)
+   {
+	   return distance(startPt, currPt) + distance(currPt, goalPt);
+   }
+   
+   public List<Point> sortOpen(List<Point> open, Point start, Point goal)
+   {
+	   List<Long> values = new LinkedList<Long>();
+	   List<Point> sorted = new LinkedList<Point>();
+	   
+	   for(Point pt: open)
+	   {
+		   values.add((long)getNodeValue(start, goal, pt));
+	   }
+	   
+	   while(!open.isEmpty() && !values.isEmpty())
+	   {
+		   int lowest = 0;
+		   for(int i = 0; i < values.size(); i++)
+		   {
+			   if(values.get(i) < values.get(lowest))
+			   {
+				   lowest = i;
+			   }
+		   }
+		   sorted.add(open.get(lowest));
+		   open.remove(lowest);
+		   values.remove(lowest);
+	   }
+	   return sorted; 
+	   
+   }
+   
+   public List<Point> getClosedSet(WorldModel world, Point startPt, Point goalPt)
+   {
+	   Point currPt = startPt; 
+	   List<Point> open = new LinkedList<Point>();
+	   List<Point> closed = new LinkedList<Point>(); 
+	   closed.add(startPt); 
+	   
+	   while(open != null)
+	   {
+		   List<Point>  temp = getAdjacentPts(world, currPt, goalPt);
+		   for(int i = 0; i < temp.size(); i++)
+		   {
+			   if(!open.contains(temp.get(i)) && !closed.contains(temp.get(i)))
+			   {
+				   open.add(temp.get(i));
+			   }
+		   }
+		   open = sortOpen(open,startPt,goalPt);
+		   
+		   currPt = open.get(0);
+		   
+		   //List<PImage> temp1 = new LinkedList<PImage>();
+		   //temp1.add(p.loadImage("../images/checkedgrass.bmp"));
+		   
+		   //world.setBackground(currPt, new Background("checked",temp1));
+ 
+		   open.remove(0); 
+		   
+		   if(!closed.contains(currPt))
+		   {
+			   closed.add(currPt);
+		   }
+		   if(currPt.x == goalPt.x && currPt.y == goalPt.y)
+		   {
+			   break; 
+		   }
+	   }
+	   return closed; 
+
+	   
+   }
+   
+   public Point traceback(WorldModel world, List<Point> closedSet, Point goal, Point start)
+   {
+	   List<Point> path = new LinkedList<Point>();
+	   if(closedSet.contains(goal))
+	   {
+		   path.add(goal);
+		   Point current = goal;
+	   
+	   
+		   while(current != start)
+		   {
+			   List<Point> temp = new LinkedList<Point>();
+			   for(Point pt: closedSet)
+			   {
+				   if(MobileAnimatedActor.adjacent(pt, current) && !path.contains(pt))
+				   {
+					   temp.add(pt);
+				   }
+			   }
+
+			   if(temp.size() != 0)
+			   {
+				   temp = sortOpen(temp,start,goal);
+				   path.add(temp.get(0));
+				   closedSet.remove(temp.get(0));
+				   current = temp.get(0);
+			   }
+			   
+		   }
+	   } 
+	   return path.get(path.size() -2);
+   }
+   
+   public Point pathFind(WorldModel world, Point startPt, Point goalPt)
+   {
+	   return traceback(world, getClosedSet(world, startPt, goalPt), goalPt, startPt);
+   }
 
    protected Point nextPosition(WorldModel world, Point dest_pt)
    {
-      int horiz = Integer.signum(dest_pt.x - getPosition().x);
-      Point new_pt = new Point(getPosition().x + horiz, getPosition().y);
 
-      if (horiz == 0 || !canPassThrough(world, new_pt))
-      {
-         int vert = Integer.signum(dest_pt.y - getPosition().y);
-         new_pt = new Point(getPosition().x, getPosition().y + vert);
-
-         if (vert == 0 || !canPassThrough(world, new_pt))
-         {
-            new_pt = getPosition();
-         }
-      }
-
+      Point new_pt = pathFind(world, getPosition(), dest_pt);
+      //System.out.println("current position: " + getPosition().x + " " + getPosition().y);
+      //System.out.println(new_pt.x + " " + new_pt.y);
+      
+      //List<PImage> temp1 = new LinkedList<PImage>();
+	   //temp1.add(p.loadImage("../images/pathgrass.bmp")); 
+	   //world.setBackground(new_pt, new Background("checked path",temp1));
+	   
       return new_pt;
    }
 
@@ -36,5 +174,5 @@ public abstract class MobileAnimatedActor
          (p1.y == p2.y && abs(p1.x - p2.x) == 1);
    }
 
-   protected abstract boolean canPassThrough(WorldModel world, Point new_pt);
+   protected abstract boolean canPassThrough(WorldModel world, Point new_pt); 
 }
